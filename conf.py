fc87290d03bcbc0849ba89c1ce2c3bd3218c8d45
masci <mpippi@gmail.com>
2013-01-06 10:07:03 +0100

    rearranged

--- conf.py
+++ conf.py
@@ -2,38 +2,34 @@
 # This is your configuration file.  Please write valid python!
 # See http://posativ.org/acrylamid/conf.py.html
 
-SITENAME = 'A descriptive blog title'
-WWW_ROOT = 'http://example.com/'
+SITENAME = 'Evonove - Software'
+WWW_ROOT = 'http://evonove.it/'
+AUTHOR = 'staff'
+EMAIL = 'info@evonove.it'
 
-AUTHOR = 'Anonymous'
-EMAIL = 'mail@example.com'
-
-FILTERS = ['markdown+codehilite(css_class=highlight)', 'hyphenate', 'h1']
 VIEWS = {
-    '/': {'filters': 'summarize', 'view': 'index',
-          'pagination': '/page/:num'},
+    '/': {'view': 'homepage', 'template': 'home.html'},
 
-    '/:year/:slug/': {'view': 'entry'},
+    #'/:year/:slug/': {'view': 'entry'},
 
-    '/tag/:name/': {'filters': 'summarize', 'view':'tag',
-                    'pagination': '/tag/:name/:num'},
+    #'/tag/:name/': {'filters': 'summarize', 'view':'tag', 'pagination': '/tag/:name/:num'},
 
     # per tag Atom or RSS feed. Just uncomment to generate them.
 
     # '/tag/:name/atom/': {'filters': ['h2', 'nohyphenate'], 'view': 'atompertag'},
     # '/tag/:name/rss/': {'filters': ['h2', 'nohyphenate'], 'view': 'rsspertag'},
 
-    '/atom/': {'filters': ['h2', 'nohyphenate'], 'view': 'atom'},
-    '/rss/': {'filters': ['h2', 'nohyphenate'], 'view': 'rss'},
+    #'/atom/': {'filters': ['h2', 'nohyphenate'], 'view': 'atom'},
+    #'/rss/': {'filters': ['h2', 'nohyphenate'], 'view': 'rss'},
 
-    '/articles/': {'view': 'articles'},
+    #'/articles/': {'view': 'articles'},
 
-    '/sitemap.xml': {'view': 'sitemap'},
+    #'/sitemap.xml': {'view': 'sitemap'},
 
     # Here are some more examples
 
     # # '/:slug/' is a slugified url of your static page's title
-    # '/:slug/': {'view': 'page'}
+    #'/:slug/': {'view': 'page'}
 
     # # '/atom/full/' will give you a _complete_ feed of all your entries
     # '/atom/full/': {'filters': 'h2', 'view': 'atom', 'num_entries': 1000},
@@ -47,6 +43,9 @@ VIEWS = {
     #                  'view': 'entry'}
 }
 
-THEME = 'theme'
+FILTERS = ['rst', 'hyphenate', 'h1']
+THEME = 'bizstrap'
 ENGINE = 'acrylamid.templates.jinja2.Environment'
 DATE_FORMAT = '%d.%m.%Y, %H:%M'
+STATIC = ['static']
+VIEWS_DIR = 'views'
