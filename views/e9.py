8d7cafc4ea3c55d8c3d717752c4308c121197c83
masci <mpippi@gmail.com>
2013-01-09 17:56:04 +0100

    to the i18n and beyond

--- views/e9.py
+++ views/e9.py
@@ -1,41 +1,33 @@
 # -*- coding: utf-8 -*-
-from acrylamid.views.entry import Translation, Base
-from acrylamid.helpers import expand, union, joinurl, event
+from acrylamid.views.entry import Translation
+from acrylamid.helpers import union, joinurl
 import os
-from os.path import isfile
-from pprint import pprint
+from datetime import datetime
+
 
 def entry_filename(entry):
     return entry.filename.split(os.path.sep)[-1]
 
 
-def entry_for_lang(request, lang, entry):
-    try:
-        for t in request.get('translations'):
-            if t.identifier == entry.identifier and t.lang == lang:
-                return t
-        return entry
-    except (AttributeError, KeyError):
-        return  entry
-
-
 class E9Base(Translation):
-    pass
-
-
-class E9Home(Translation):
     def __init__(self, *args, **kw):
         Translation.__init__(self, *args, **kw)
+        self.langs = set()
 
-    def generate(self, request):
-        content_dir = os.path.abspath(self.conf['content_dir'])
+    def _entry_for_lang(self, request, lang, entry):
+        try:
+            for t in request['translations']:
+                if t.identifier == entry.identifier and t.lang == lang:
+                    return t
+            return entry
+        except (AttributeError, KeyError):
+            return  entry
 
+    def _populate_entries(self, request, lang=None):
         entry_dict = {}
-        #request['entrylist'] = []
-        #pprint(request)
-
         for e in request['entrylist']:
-            e = entry_for_lang(request, 'en', e)
+            if lang:
+                e = self._entry_for_lang(request, lang, e)
             if 'banners' in e.filename.split(os.path.sep):
                 entry_dict.setdefault('banners', []).append(e)
             elif 'hero-list' in e.filename.split(os.path.sep):
@@ -43,35 +35,36 @@ class E9Home(Translation):
             else:
                 entry_dict[e.slug] = e
 
-        request['env']['entry_dict'] = entry_dict
-        request['env']['path'] = '/'
-        path = joinurl(self.conf['output_dir'], 'en', 'index.html')
-        tt = self.env.engine.fromfile(self.template)
-        html = tt.render(conf=self.conf, env=union(self.env,
-            type=self.__class__.__name__.lower()))
+        return entry_dict
 
-        yield html, path
+    def context(self, env, request):
+        for t in request['translations'][:]:
+            self.langs.add(t.lang)
+
+        env = Translation.context(self, env, request)
+        env.current_year = datetime.now().year
+        return env
 
 
-class E9HomeIt(Translation):
-    def __init__(self, *args, **kw):
-        Translation.__init__(self, *args, **kw)
-
+class E9Home(E9Base):
     def generate(self, request):
-        entry_dict = {}
-
-        for e in request['entrylist']:
-            if 'banners' in e.filename.split(os.path.sep):
-                entry_dict.setdefault('banners', []).append(e)
-            elif 'hero-list' in e.filename.split(os.path.sep):
-                entry_dict.setdefault('hero_list', []).append(e)
-            else:
-                entry_dict[e.slug] = e
-
-        request['env']['entry_dict'] = entry_dict
+        request['env']['entry_dict'] = self._populate_entries(request)
         path = joinurl(self.conf['output_dir'], '/index.html')
         tt = self.env.engine.fromfile(self.template)
         html = tt.render(conf=self.conf, env=union(self.env,
             type=self.__class__.__name__.lower()))
 
         yield html, path
+
+
+class E9Homei18n(E9Base):
+    def generate(self, request):
+        for lang in self.langs:
+            request['env']['entry_dict'] = self._populate_entries(request,lang)
+            request['env']['path'] = '/'
+            path = joinurl(self.conf['output_dir'], lang, 'index.html')
+            tt = self.env.engine.fromfile(self.template)
+            html = tt.render(conf=self.conf, env=union(self.env,
+                type=self.__class__.__name__.lower()))
+
+            yield html, path
